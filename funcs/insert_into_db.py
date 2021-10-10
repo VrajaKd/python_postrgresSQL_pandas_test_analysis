4b97d79b043d7bcbc9f2c0ba7bb1be1bfe3c14ad
Virko Kask <virkokask@gmail.com>
2021-10-10 12:23:54 +0300

    Added db data types

--- funcs/insert_into_db.py
+++ funcs/insert_into_db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, Float, String
 import io
 import os
 from dotenv import load_dotenv
@@ -15,17 +16,33 @@ def insert_into_db(df, table_name):
                            os.environ.get('POSTGRES_PORT') + '/' +
                            os.environ.get('POSTGRES_DB'))
 
-    # Drop old table and create new empty table
+    # Reset df index from 0
+    df = df.reset_index(drop=True)
+
     try:
-        df.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
+        # Insert db head and metadata
+        dtype = {'sequence': Integer(), 'tag_id': String, 'tag_timestamp': Integer, 'move':String, 'x_coord': Float, 'y_coord': Float, 'z_coord': Float, 'time': Float}
+        for column_name in list(df):
+            if column_name.find('anchor') != -1:
+                dtype[column_name] = String
+            if column_name.find('dist_') != -1:
+                dtype[column_name] = Integer
+            if column_name.find('_move') != -1:
+                dtype[column_name] = String
+
+        # Drop old table and create new empty table
+        df.head(0).to_sql(table_name, engine, if_exists='replace', index=True, index_label='id', dtype=dtype)
 
         conn = engine.raw_connection()
         cur = conn.cursor()
+
+        # Insert into the db
         output = io.StringIO()
-        df.to_csv(output, sep='\t', header=False, index=False)
+        df.to_csv(output, sep='\t', header=False, index=True)
         output.seek(0)
         cur.copy_from(output, table_name, null="")  # Null values become ''
         conn.commit()
+
         print(f'Data entered into the database table "{table_name}"...')
         return 'ok'
     except Exception as e:
